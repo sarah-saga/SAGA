24ee2b36fb9a8100febc8cec63e9d26ca2ba9454
sarah-saga <sarah@agence-saga.fr>
2024-08-13 16:44:02 +0200

    2

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -19,21 +19,24 @@
  * @package WordPress
  */
 
-// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
-/** Nom de la base de données de WordPress. */
-define('DB_NAME', 'wordpress_db');
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'database_name_here' );
 
-/** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+/** Database username */
+define( 'DB_USER', 'username_here' );
 
-/** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'root');
+/** Database password */
+define( 'DB_PASSWORD', 'password_here' );
 
-/** Adresse de l’hébergement MySQL. */
-define('DB_HOST', 'agence-saga.fr');
+/** Database hostname */
+define( 'DB_HOST', 'agence-saga.fr' );
 
-/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
-define('DB_CHARSET', 'utf8');
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
 
 /** Type de collation de la base de données.
   * N’y touchez que si vous savez ce que vous faites.
